bbb00c486b55deb8f33313c60d05110809ee816e
super <416785947@qq.com>
2026-01-30 22:42:49 +0800

    更新cookie和header

--- lib/httpp.py
+++ lib/httpp.py
@@ -8,6 +8,7 @@ import time
 import requests
 import json
 import traceback
+import random
 from . import log
 
 log = log.Log()
@@ -15,28 +16,49 @@ log = log.Log()
 
 class Http(object):
 
-    def __init__(self):
-        cookies = {}
-        cookies['UID'] = '16F72a24619110367ed665c1523413673'
-        cookies['UIDR'] = '1523413673'
+    def __init__(self, cookies=None):
+        if cookies:
+            self.cookies = cookies
+        else:
+            cookies = {}
+            cookies['UID'] = '16F72a24619110367ed665c1523413673'
+            cookies['UIDR'] = '1523413673'
+            self.cookies = cookies
         self.timeOutLimit = 10
-        self.cookies = cookies
         self.headers = {
-            'User-Agent': 'User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
+            'User-Agent': self._generate_random_user_agent()}
         pass
+    
+    def _generate_random_user_agent(self):
+        platforms = ['Windows NT 10.0', 'Windows NT 6.1', 'Windows NT 6.3', 'Macintosh; Intel Mac OS X 10_15_7', 'Macintosh; Intel Mac OS X 10_14_6', 'X11; Linux x86_64']
+        browsers = [
+            ('Chrome', '91.0.4472.124', 'AppleWebKit/537.36', 'KHTML, like Gecko', 'Safari/537.36'),
+            ('Firefox', '89.0', 'Gecko/20100101'),
+            ('Safari', '14.1.1', 'AppleWebKit/605.1.15', 'KHTML, like Gecko')
+        ]
+        
+        platform = random.choice(platforms)
+        browser_info = random.choice(browsers)
+        
+        if browser_info[0] == 'Chrome':
+            return f'Mozilla/5.0 ({platform}) {browser_info[2]} ({browser_info[3]}) {browser_info[0]}/{browser_info[1]} {browser_info[4]}'
+        elif browser_info[0] == 'Firefox':
+            return f'Mozilla/5.0 ({platform}; rv:{browser_info[1]}) {browser_info[2]} {browser_info[0]}/{browser_info[1]}'
+        else:  # Safari
+            return f'Mozilla/5.0 ({platform}) {browser_info[2]} ({browser_info[3]}) Version/{browser_info[1]} {browser_info[0]}/{browser_info[1]}'
 
     def get(self, url, paramsDict=None):
         if not url:
             log.error("get fail. url is empty")
             return False
         try:
-            log.info("url:%s params:%s" % (url, json.dumps(paramsDict)))
+            log.info("get url:%s params:%s" % (url, json.dumps(paramsDict)))
             if paramsDict:
-                r = requests.get(url, headers=self.headers, timeout=self.timeOutLimit, params=paramsDict)
+                r = requests.get(url, headers=self.headers, cookies=self.cookies, timeout=self.timeOutLimit, params=paramsDict)
             else:
-                r = requests.get(url, headers=self.headers, timeout=self.timeOutLimit)
+                r = requests.get(url, headers=self.headers, cookies=self.cookies, timeout=self.timeOutLimit)
 
-            log.info("get ret:%s url:%s params:%s" % (r.text, url, json.dumps(paramsDict)))
+            #log.info("get ret:%s url:%s params:%s" % (r.text, url, json.dumps(paramsDict)))
             if r.headers['Content-Type'].find('application/json') != -1 or r.headers['content-type'].find(
                     'application/json') != -1:
                 return r.json()
@@ -53,13 +75,13 @@ class Http(object):
             log.error("post fail. url is empty")
             return False
         try:
-            log.info("url:%s params:%s" % (url, json.dumps(paramsDict)))
+            log.info("post url:%s params:%s" % (url, json.dumps(paramsDict)))
             if paramsDict:
-                r = requests.post(url, timeout=self.timeOutLimit, data=paramsDict)
+                r = requests.post(url, headers=self.headers, cookies=self.cookies, timeout=self.timeOutLimit, data=paramsDict)
             else:
-                r = requests.post(url, timeout=self.timeOutLimit)
+                r = requests.post(url, headers=self.headers, cookies=self.cookies, timeout=self.timeOutLimit)
 
-            log.info("post ret:%s url:%s params:%s" % (r.text, url, json.dumps(paramsDict)))
+            #log.info("post ret:%s url:%s params:%s" % (r.text, url, json.dumps(paramsDict)))
             if r.headers['Content-Type'].find('application/json') != -1 or r.headers['content-type'].find(
                     'application/json') != -1:
                 return r.json()
